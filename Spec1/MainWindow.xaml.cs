fa65264d00e4a74f4375803817affd3198fdd5b9
juziem <lus0800@mail.ru>
2019-09-15 19:56:41 +0700

    1

---
+++ Spec1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Spec1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //public class Fish
+        //{
+        //    int speed, energy, x, y;
+
+        //    public void move (int x, int y)
+        //    {
+                
+        //    }
+
+        //    public int age()
+        //    {
+        //        int a = 0;
+        //        return a;
+        //    }
+        //}
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string s=Console.ReadLine();
+
+
+            Console.WriteLine(s);
+        }
+    }
+}
